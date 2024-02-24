6947fee1eba27b365e2d6ec1e91ed24225ed418b
AndreaCheah <104676203+AndreaCheah@users.noreply.github.com>
2024-02-24 16:37:52 +0100

    Add input validation such that non-numeric characters are not allowed

--- src/problem2/script.js
+++ src/problem2/script.js
@@ -1,26 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
   const currencySelectElements = document.querySelectorAll(".currency-select");
-
   currencySelectElements.forEach((selectElement) => {
-      fetch("https://interview.switcheo.com/prices.json")
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          const currencies = new Set();
-          data.forEach((item) => {
-            console.log(item.currency);
-            if (!currencies.has(item.currency)) {
-              currencies.add(item.currency);
-              const option = document.createElement("option");
-              option.value = item.currency;
-              option.text = item.currency;
-              selectElement.appendChild(option);
-            }
-          });
-        })
-        .catch((error) =>
-          console.error("Error fetching currency list:", error)
-        );
+    fetch("https://interview.switcheo.com/prices.json")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const currencies = new Set();
+        data.forEach((item) => {
+          console.log(item.currency);
+          if (!currencies.has(item.currency)) {
+            currencies.add(item.currency);
+            const option = document.createElement("option");
+            option.value = item.currency;
+            option.text = item.currency;
+            selectElement.appendChild(option);
+          }
+        });
+      })
+      .catch((error) => console.error("Error fetching currency list:", error));
+  });
+
+  document.querySelectorAll("input").forEach(function (input) {
+    input.addEventListener("keypress", function (e) {
+      var allowedChars = "0123456789.";
+      function contains(stringValue, charValue) {
+        return stringValue.indexOf(charValue) > -1;
+      }
+      var invalidKey =
+        (e.key.length === 1 && !contains(allowedChars, e.key)) ||
+        (e.key === "." && contains(e.target.value, "."));
+      invalidKey && e.preventDefault();
+    });
   });
   0;
 });
